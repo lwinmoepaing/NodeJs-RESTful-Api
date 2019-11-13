4559bf2d1387388c15d22e63c6ecd49a8a716c2f
Lwin Moe Paing <lwinmoepaing.dev@gmail.com>
2019-11-14 00:47:49 +0630

    First Create Course

---
+++ 0_Javascript_ES6/0_1_JS_Object.js
@@ -0,0 +1,61 @@
+/**
+|--------------------------------------------------
+| What Is Javascript Object ?
+|--------------------------------------------------
+*/
+
+let obj = {
+	name: 'Kyaw Kyaw',
+	age: 20,
+	role: 'student',
+	books: [ 'Book 1', 'Book 2', 'Book 3' ]
+};
+
+console.log(obj);
+
+/**
+|--------------------------------------------------
+| Get Each Data
+|--------------------------------------------------
+*/
+
+// Get Name
+console.log(obj.name);
+// Get Age
+console.log(obj.age);
+// Get Role
+console.log(obj.role);
+// Get Books Array
+console.log(obj.books);
+
+/**
+|--------------------------------------------------
+| Exercise For JavaScript Object
+|--------------------------------------------------
+| to make new Object
+| @description This will be person & describe below
+| @name "Jack"
+| @age "20"
+| @birthdate will object and it have 
+|       @day "23"
+|       @month "03"
+|       @year "1996"
+*/
+
+// Here Make Object
+
+//
+//
+//
+
+/**
+|--------------------------------------------------
+| Get each data From new Object
+|--------------------------------------------------
+*/
+
+// Here Get Object
+
+//
+//
+//
---
+++ README.md
@@ -0,0 +1,18 @@
+#Node Js Restful Api 
+
+###Setup Environment
+
+1.Web Technology (Http/Https)
+1.Http Status Code
+
+###Course Outline
+
+1.Introduction Overview
+1.Preparing JavaScript(ES6)
+1.Make *CRUD* with *Express*
+1.Mongo DB
+1.Secure Validation
+1.Deploy (Heroku)
+1.Extra ( ViewTemplate, FileUploading, Authorization)
+
+https://docs.google.com/presentation/d/16h-FhPRDwKMiToM5_MMKmbn2wlMDxC0Bp7CRVg25XbU/edit?usp=sharing
